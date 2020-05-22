f75ef0b43f692e68d8e6e4c831d00c9c2dc968f1
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-05-21 22:15:08 -0300

    Refatorando o código com base no Code Review

--- modulo_2/main.py
+++ modulo_2/main.py
@@ -11,10 +11,11 @@ class Employee(metaclass=ABCMeta):
     work_hours = 8
 
     @abstractmethod
-    def __init__(self, code, name, salary):
+    def __init__(self, code, name, salary, departament):
         self.code = code
         self.name = name
         self.__salary = salary
+        self.__departament = departament
 
     @abstractmethod
     def calc_bonus(self):
@@ -32,36 +33,28 @@ class Employee(metaclass=ABCMeta):
     def salary(self, salary):
         self.__salary = salary
 
+    def get_departament(self):
+        return self.__departament.name
+
+    def set_departament(self, departament):
+        self.__departament.name = departament
+
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('managers', 1)
+        super().__init__(code, name, salary, Department('managers', 1))
 
     def calc_bonus(self):
         return self.salary * 0.15
 
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_departament(self, departament):
-        self.__departament.name = departament
-
 
 class Seller(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.__departament = Department('sellers', 2)
+        super().__init__(code, name, salary, Department('sellers', 2))
         self.__sales = 0
 
     def calc_bonus(self):
         return self.__sales * 0.15
-
-    def get_departament(self):
-        return self.__departament.name
-
-    def set_departament(self, departament):
-        self.__departament.name = departament
 
     def get_sales(self):
         return self.__sales
